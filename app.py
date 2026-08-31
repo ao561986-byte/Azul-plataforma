8efdd499cb0403683f4dd7ebb8141a4718759f12
ao561986-byte <ao561986@gmail.com>
2026-08-31 19:30:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
 
+<main>
+
+    <div class="card">
+        <h2>Bienvenido</h2>
+        <p>
+            La plataforma está funcionando correctamente.
+        </p>
+
+        <a class="boton" href="/salud">
+            Comprobar sistema
+        </a>
+    </div>
+
+    <div class="card">
+        <h2>Panel</h2>
+        <p>
+            Desde aquí podremos agregar los productos,
+            costos, ventas y análisis de rentabilidad.
+        </p>
+    </div>
+
+</main>
+
+</body>
+</html
