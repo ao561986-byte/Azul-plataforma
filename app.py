2ceba79c573d3ac78dd37b37e50410f37522aa46
ao561986-byte <ao561986@gmail.com>
2026-08-31 21:11:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,14 @@
-
-Escritura
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def inicio():
     return render_template("index.html")
 
-
 @app.route("/salud")
 def salud():
     return "Azul Plataforma funcionando correctamente."
 
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-Nombre del archivo: Aplicacion.py
-Ubicación: directamente dentro de Azul-plataforma, no dentro de templates.
-La estructura Azul-plataforma/
-├── app.py
-├── requirements.txt
-├── Procfile
-└── templates/
-    └── index.html
