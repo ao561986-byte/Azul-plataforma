c2454224c1ff3786a008e360b3ed08c20b8e63e1
ao561986-byte <ao561986@gmail.com>
2026-09-01 14:23:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -93,4 +94,3 @@ def products():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
-
