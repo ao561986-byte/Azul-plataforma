b30442b3bc88e68606cd2adbfe8e6f088647ef46
ao561986-byte <ao561986@gmail.com>
2026-08-31 21:20:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def inicio():
     return render_template("index.html")
 
+
 @app.route("/salud")
 def salud():
     return "Azul Plataforma funcionando correctamente."
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
